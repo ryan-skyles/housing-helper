631840e40be7d2f6db7920f1f8a8451d9e8f8537
ryan-skyles <ryan.skyles01@gmail.com>
2026-02-21 12:04:59 -0700

    Screensize Adjustments

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,10 +4,12 @@ import WavyFooter from "./WavyFooter";
 
 const AppLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-background max-w-md mx-auto relative shadow-lg">
+    <div className="min-h-screen w-full flex flex-col bg-background relative">
       <AppHeader />
-      <main className="flex-1 flex flex-col">
-        <Outlet />
+      <main className="flex-1 flex flex-col items-center">
+        <div className="w-full max-w-[600px] px-4">
+          <Outlet />
+        </div>
       </main>
       <WavyFooter />
     </div>
